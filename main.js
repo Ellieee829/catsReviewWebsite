9a0fa914705def309249db43914fd1f2b20cf503
Wilson <10wilson1999@gmail.com>
2025-09-18 21:57:02 +0800

    Update main.js from contrib branch

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss";
 
-console.log('Hello world');
+/* popover 跳出式視窗初始化 */
+document.addEventListener("DOMContentLoaded", () => {
+  const popoverTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="popover"]'
+  );
+  [...popoverTriggerList].map(
+    (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+  );
+});
+
+console.log("Hello world");
