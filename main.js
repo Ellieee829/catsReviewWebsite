f4eb0090c0cd9c039e9bf65e3c8848f8a169c814
Wilson <10wilson1999@gmail.com>
2025-09-18 19:11:31 +0800

    contrib branch dev

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss";
+import * as bootstrap from "bootstrap";
 
-console.log('Hello world');
+/* popover 跳出式視窗初始化 */
+document.addEventListener("DOMContentLoaded", () => {
+  const popoverTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="popover"]'
+  );
+  [...popoverTriggerList].map(
+    (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+  );
+});
+
+console.log("Hello world");
