b34da9d001d307b61b697941cc278cfe91ab83cd
Wilson <10wilson1999@gmail.com>
2025-10-01 16:34:28 +0800

    feat: contrib food page RWD

--- main.js
+++ main.js
@@ -19,43 +19,60 @@ const container = document.getElementById("extra-container");
 // 監聽新增按鈕
 addBtn.addEventListener("click", () => {
   const newItem = document.createElement("div");
-  newItem.className = "mb-8 d-flex extra-item";
+  newItem.className = "mb-md-8 mb-sm-3 d-sm-flex extra-item";
   newItem.innerHTML = `
-      <label class="me-6 w-100">
+      <label for="extraName" class="me-sm-6 mb-3 mb-sm-0 w-100">
         <div>
-          <p class="px-2 d-flex mb-2">
-            <span class="contrib-font-size-sm neutral-900">額外成分名稱</span>
-            <span class="contrib-font-size-xs neutral-600 ms-auto">選填</span>
+          <p class="px-2 d-flex mb-1 mb-md-2">
+            <span class="contrib-font-size-sm neutral-900"
+              >額外成分名稱</span
+            >
+            <span class="contrib-font-size-xs neutral-600 ms-auto"
+              >選填</span
+            >
           </p>
           <div>
             <input
               type="text"
-              class="form-control border-radius contrib-input-heigh"
+              class="form-control border-radius contrib-input-heigh px-4"
+              id="extraName"
               placeholder="請輸入額外成分（例：牛磺酸）"
             />
           </div>
         </div>
       </label>
       <div class="w-100 d-flex align-items-end">
-        <label class="w-100 me-6">
-          <p class="px-2 d-flex mb-2">
-            <span class="contrib-font-size-sm neutral-900">額外成分比例</span>
-            <span class="contrib-font-size-xs neutral-600 ms-auto">選填</span>
+        <label for="extra%" class="w-100 me-6 mb-3 mb-sm-0">
+          <p class="px-2 d-flex mb-1 mb-md-2">
+            <span class="contrib-font-size-sm neutral-900"
+              >額外成分比例</span
+            >
+            <span class="contrib-font-size-xs neutral-600 ms-auto"
+              >選填</span
+            >
           </p>
           <div class="input-group">
             <input
               type="text"
-              class="form-control border-radius contrib-input-heigh"
+              class="form-control border-radius contrib-input-heigh px-4"
+              id="extra%"
               placeholder="請輸入額外成分比例"
             />
-            <div class="input-group-text neutral-800 bg-neutral-100">%</div>
+            <div
+              class="input-group-text neutral-800 bg-neutral-100"
+            >
+              %
+            </div>
           </div>
         </label>
         <button
           type="button"
-          class="btn contrib-delete-area contrib-delete-btn border-radius"
+          class="btn contrib-delete-area contrib-delete-btn border-radius mb-3 mb-sm-0"
         >
-          <img src="../assets/images/contrib/trash.png" alt="trash" />
+          <img
+            src="../assets/images/contrib/trash.png"
+            alt="trash"
+          />
         </button>
       </div>
     `;
